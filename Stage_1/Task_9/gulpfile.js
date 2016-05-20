2a8066605502dae9161d0939a6fd72baa6515bc5
DwayneTen <151809668@qq.com>
2016-05-20 22:09:41 +0800

    + Add task9 basic layout.

---
+++ Stage_1/Task_9/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+var rename = require('gulp-rename');
+var cssmin = require('gulp-cssmin');
+
+var lesDist= './less/**/*.less';
+var cssDist = './css';
+
+gulp.task('less', function () {
+    return gulp.src(lesDist)
+        .pipe(watch(lesDist))
+        .pipe(less())
+        .pipe(gulp.dest(cssDist))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cssmin())
+        .pipe(gulp.dest(cssDist));
+});
