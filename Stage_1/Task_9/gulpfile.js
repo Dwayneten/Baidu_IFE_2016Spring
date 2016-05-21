3383ed56dba56a5cc19703409802d8c20520dc7f
DwayneTen <151809668@qq.com>
2016-05-21 11:50:31 +0800

    * Finish task9 header.

--- Stage_1/Task_9/gulpfile.js
+++ Stage_1/Task_9/gulpfile.js
@@ -4,12 +4,12 @@ var watch = require('gulp-watch');
 var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
 
-var lesDist= './less/**/*.less';
+var lesDist = './less/**/*.less';
 var cssDist = './css';
 
 gulp.task('less', function () {
     return gulp.src(lesDist)
-        .pipe(watch(lesDist))
+        // .pipe(watch(lesDist))
         .pipe(less())
         .pipe(gulp.dest(cssDist))
         .pipe(rename({ suffix: '.min' }))
